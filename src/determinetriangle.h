794fd036c38872f379f8601ae51ad49a2f50ce32
Usagitoneko <hogouxian@gmail.com>
2017-06-05 16:48:29 +0800

    added git ignore

--- src/determinetriangle.h
+++ src/determinetriangle.h
@@ -6,6 +6,7 @@ struct TriangleSides{
   int short1, short2, long1;
 };
 typedef struct TriangleSides TriangleSides;
+
 char* determine_type_and_validity_triangle(int v1, int v2, int v3);
 TriangleSides arrange_ascending( int v1, int v2, int v3);
 char* get_triangle_type(TriangleSides aTriangle);
